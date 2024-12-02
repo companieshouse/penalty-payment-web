e12e0d749d7f24309d1adbc65cb8bae115a8bcca
dgordon-ch <dgordon@companieshouse.gov.uk>
2024-12-02 09:26:25 +0000

    SAN-152 - Revert CSRF token changes for signin testing

--- src/main/java/uk/gov/companieshouse/web/pps/security/WebSecurity.java
+++ src/main/java/uk/gov/companieshouse/web/pps/security/WebSecurity.java
@@ -19,8 +19,9 @@ public class WebSecurity {
     @Bean
     @Order(1)
     public SecurityFilterChain temporaryStartPageSecurityFilterChain(HttpSecurity http) throws Exception {
-        return ChsCsrfMitigationHttpSecurityBuilder.configureWebCsrfMitigations(
-                http.securityMatcher("/late-filing-penalty")).build();
+        http
+                .securityMatcher("/late-filing-penalty");
+        return http.build();
     }
 
     @Bean
@@ -33,16 +34,18 @@ public class WebSecurity {
 
     @Bean
     @Order(3)
-        protected SecurityFilterChain accessibilityStatementPageSecurityConfig(HttpSecurity http) throws Exception {
-        return ChsCsrfMitigationHttpSecurityBuilder.configureWebCsrfMitigations(
-                http.securityMatcher("/late-filing-penalty/accessibility-statement")).build();
+    protected SecurityFilterChain accessibilityStatementPageSecurityConfig(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/late-filing-penalty/accessibility-statement");
+        return http.build();
     }
 
     @Bean
     @Order(4)
-        protected SecurityFilterChain healthcheckSecurityFilterChain(HttpSecurity http) throws Exception {
-        return ChsCsrfMitigationHttpSecurityBuilder.configureApiCsrfMitigations(
-                http.securityMatcher("/late-filing-penalty/healthcheck")).build();
+    protected SecurityFilterChain healthcheckSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/late-filing-penalty/healthcheck");
+        return http.build();
     }
 
     @Bean
@@ -54,13 +57,15 @@ public class WebSecurity {
 
     @Bean
     @Order(6)
-        public SecurityFilterChain ppsWebSecurityFilterConfig(HttpSecurity http) throws Exception {
-        return ChsCsrfMitigationHttpSecurityBuilder.configureWebCsrfMitigations(http
+    public SecurityFilterChain lFPWebSecurityFilterConfig (HttpSecurity http) throws Exception {
+        http
                 .securityMatcher("/late-filing-penalty/**")
                 .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
                 .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class)).build();
-        }
+                .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
 
 }
 
--- src/test/java/uk/gov/companieshouse/web/pps/security/WebsecurityTests.java
+++ src/test/java/uk/gov/companieshouse/web/pps/security/WebsecurityTests.java
@@ -3,7 +3,6 @@ package uk.gov.companieshouse.web.pps.security;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,11 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
-import uk.org.webcompere.systemstubs.jupiter.SystemStub;
-import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@ExtendWith(SystemStubsExtension.class)
 @ExtendWith(MockitoExtension.class)
 class WebsecurityTests {
 
@@ -25,31 +20,13 @@ class WebsecurityTests {
     @InjectMocks
     private WebSecurity webSecurity;
 
-    @SystemStub
-    private EnvironmentVariables environmentVariables;
-
-    @BeforeEach
-    public void beforeEach(){
-        environmentVariables.set("COOKIE_NAME", "__SID");
-        environmentVariables.set("COOKIE_DOMAIN", "chs.local");
-    }
 
     @Test
     @DisplayName(" apply security filter to /late-filing-penalty")
     void temporaryStartPageSecurityFilterChainTest() throws Exception {
-       when(httpSecurity.securityMatcher("/late-filing-penalty")).thenReturn(httpSecurity);
-       assertEquals(webSecurity.temporaryStartPageSecurityFilterChain(httpSecurity), httpSecurity.build());
+        when(httpSecurity.securityMatcher("/late-filing-penalty")).thenReturn(httpSecurity);
+        assertEquals(webSecurity.temporaryStartPageSecurityFilterChain(httpSecurity), httpSecurity.build());
     }
-
-    @Test
-    @DisplayName(" apply security filter to /late-filing-penalty/ref-starts-with")
-    void penaltyRefStartsWithPageSecurityFilterChainTest() throws Exception {
-        when(httpSecurity.securityMatcher("/late-filing-penalty/ref-starts-with"))
-                .thenReturn(httpSecurity);
-        assertEquals(webSecurity.penaltyRefStartsWithPageSecurityFilterChain(httpSecurity),
-                httpSecurity.build());
-    }
-
     @Test
     @DisplayName(" apply security filter to /late-filing-penalty/accessibility-statement")
     void accessibilityStatementPageSecurityConfigTest() throws Exception {
@@ -62,6 +39,15 @@ class WebsecurityTests {
     void healthcheckSecurityFilterChainTest() throws Exception {
         when(httpSecurity.securityMatcher("/late-filing-penalty/healthcheck")).thenReturn(httpSecurity);
         assertEquals(webSecurity.healthcheckSecurityFilterChain(httpSecurity), httpSecurity.build());
+    }
+
+    @Test
+    @DisplayName(" apply security filter to /late-filing-penalty/ref-starts-with")
+    void penaltyRefStartsWithPageSecurityFilterChainTest() throws Exception {
+        when(httpSecurity.securityMatcher("/late-filing-penalty/ref-starts-with"))
+                .thenReturn(httpSecurity);
+        assertEquals(webSecurity.penaltyRefStartsWithPageSecurityFilterChain(httpSecurity),
+                httpSecurity.build());
     }
 
     @Test
