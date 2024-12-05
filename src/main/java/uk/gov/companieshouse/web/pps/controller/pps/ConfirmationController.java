6ded650f3afb854164b0ae48f9fc939aca27735d
klai-ch <klai@companieshouse.gov.uk>
2024-12-05 15:41:16 +0000

    SAN-24 UpdateConfirmation page

--- src/main/java/uk/gov/companieshouse/web/pps/controller/pps/ConfirmationController.java
+++ src/main/java/uk/gov/companieshouse/web/pps/controller/pps/ConfirmationController.java
@@ -58,7 +58,7 @@ public class ConfirmationController extends BaseController {
     public String getConfirmation(@PathVariable String companyNumber,
                                   @PathVariable String penaltyId,
                                   @RequestParam("ref") Optional<String> reference,
-//                                  @RequestParam("state") String paymentState,
+                                  @RequestParam("state") String paymentState,
                                   @RequestParam("status") String paymentStatus,
                                   HttpServletRequest request,
                                   Model model) {
@@ -66,26 +66,24 @@ public class ConfirmationController extends BaseController {
         Map<String, Object> sessionData = sessionService.getSessionDataFromContext();
 
         // Check that the session state is present
-//        if (!sessionData.containsKey(PAYMENT_STATE)) {
-//            LOGGER.errorRequest(request, "Payment state value is not present in session, Expected: " + paymentState);
-//            return ERROR_VIEW;
-//        }
+        if (!sessionData.containsKey(PAYMENT_STATE)) {
+            LOGGER.errorRequest(request, "Payment state value is not present in session, Expected: " + paymentState);
+            return ERROR_VIEW;
+        }
 
-//        String sessionPaymentState = (String) sessionData.get(PAYMENT_STATE);
-//        sessionData.remove(PAYMENT_STATE);
+        String sessionPaymentState = (String) sessionData.get(PAYMENT_STATE);
+        sessionData.remove(PAYMENT_STATE);
 
         // Check that the session state has not been tampered with
-//        if (!paymentState.equals(sessionPaymentState)) {
-//            LOGGER.errorRequest(request, "Payment state value in session is not as expected, possible tampering of session "
-//                    + "Expected: " + sessionPaymentState + ", Received: " + paymentState);
-//            return ERROR_VIEW;
-//        }
+        if (!paymentState.equals(sessionPaymentState)) {
+            LOGGER.errorRequest(request, "Payment state value in session is not as expected, possible tampering of session "
+                    + "Expected: " + sessionPaymentState + ", Received: " + paymentState);
+            return ERROR_VIEW;
+        }
 
         // If the payment is anything but paid return user to beginning of journey
         PayableLateFilingPenalty payableLateFilingPenalty;
         CompanyProfileApi companyProfileApi;
-        penaltyId = "AL61174690";
-        paymentStatus = "paid";
         try {
             companyProfileApi = companyService.getCompanyProfile(companyNumber);
             payableLateFilingPenalty = payablePenaltyService
