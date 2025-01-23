bfc6a2becc1b69f4f0ae308375c0cd97c29eb874
klai-ch <klai@companieshouse.gov.uk>
2025-01-23 12:56:09 +0000

    SAN-138 Set up for Sanctions part

--- src/main/java/uk/gov/companieshouse/web/pps/controller/pps/OnlinePaymentUnavailableController.java
+++ src/main/java/uk/gov/companieshouse/web/pps/controller/pps/OnlinePaymentUnavailableController.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.web.pps.controller.pps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +8,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.web.pps.annotation.PreviousController;
 import uk.gov.companieshouse.web.pps.controller.BaseController;
+import uk.gov.companieshouse.web.pps.util.PenaltyReference;
+import uk.gov.companieshouse.web.pps.util.PenaltyUtils;
 
 @Controller
 @PreviousController(EnterDetailsController.class)
 @RequestMapping("/late-filing-penalty/company/{companyNumber}/penalty/{penaltyNumber}/online-payment-unavailable")
 public class OnlinePaymentUnavailableController extends BaseController {
 
+    private final PenaltyUtils penaltyUtils;
+
     private static final String ONLINE_PAYMENT_UNAVAILABLE = "pps/onlinePaymentUnavailable";
+
+    private static final String PENALTY_REFERENCE_MODEL_ATTR = "penaltyReference";
 
     @Override protected String getTemplateName() {
         return ONLINE_PAYMENT_UNAVAILABLE;
+    }
+
+    @Autowired
+    public OnlinePaymentUnavailableController(PenaltyUtils penaltyUtils){
+        this.penaltyUtils = penaltyUtils;
     }
 
     @GetMapping
@@ -24,9 +36,18 @@ public class OnlinePaymentUnavailableController extends BaseController {
                                               @PathVariable String penaltyNumber,
                                               Model model) {
 
-        addBaseAttributesToModel(model);
+        String penaltyReferenceStart = penaltyNumber.substring(0, 1);
+        try {
+            PenaltyReference penaltyReference = PenaltyReference.fromStartsWith(penaltyReferenceStart);
 
-        return getTemplateName();
+            model.addAttribute(PENALTY_REFERENCE_MODEL_ATTR, penaltyReference.toString());
+            addBaseAttributesToModel(model);
+
+            return getTemplateName();
+        } catch (IllegalArgumentException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            return penaltyUtils.getUnscheduledServiceDownPath();
+        }
     }
 
 }
