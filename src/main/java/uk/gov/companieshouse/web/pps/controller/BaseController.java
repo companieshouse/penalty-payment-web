04db8c044f8bb8e13accf5da8925214213f4ee41
aonubeze-ch <aonubeze@companieshouse.gov.uk>
2025-02-20 11:59:32 +0000

    SAN-292 use govuk-frontend v5.8.0 from static resources

--- src/main/java/uk/gov/companieshouse/web/pps/controller/BaseController.java
+++ src/main/java/uk/gov/companieshouse/web/pps/controller/BaseController.java
@@ -93,7 +93,7 @@ public abstract class BaseController {
     }
 
     protected void addPhaseBannerToModel(Model model, String surveyLink) {
-        model.addAttribute(PHASE_BANNER_ATTR, "beta");
+        model.addAttribute(PHASE_BANNER_ATTR, "Beta");
         model.addAttribute(PHASE_BANNER_LINK_ATTR, surveyLink);
     }
 
