eac823abe5d781ff3c4ff8117489157019f17441
klyttle-ch <klyttle@companieshouse.gov.uk>
2025-07-11 15:08:03 +0100

    SAN-515-SignOutController-Refactor

--- src/main/java/uk/gov/companieshouse/web/pps/controller/pps/SignOutController.java
+++ src/main/java/uk/gov/companieshouse/web/pps/controller/pps/SignOutController.java
@@ -17,7 +17,6 @@ import uk.gov.companieshouse.web.pps.service.signout.SignOutService;
 import uk.gov.companieshouse.web.pps.session.SessionService;
 
 import java.util.Map;
-import java.util.Optional;
 
 import static org.springframework.web.servlet.view.UrlBasedViewResolver.REDIRECT_URL_PREFIX;
 
@@ -54,17 +53,14 @@ public class SignOutController extends BaseController {
         }
 
         LOGGER.debug("Processing sign out");
-        Optional.ofNullable(signOutService.resolveBackLink(request))
-                .ifPresentOrElse(
-                        backLink -> {
-                            model.addAttribute(BACK_LINK, backLink);
-                            LOGGER.info("Referer is " + backLink);
-                        },
-                        () -> {
-                            model.addAttribute(BACK_LINK, "/pay-penalty/");
-                            LOGGER.info("Refer is sign-out or missing - fallback applied");
-                        }
-                );
+        String backLink = signOutService.resolveBackLink(request);
+        if (backLink != null) {
+            model.addAttribute(BACK_LINK, backLink);
+            LOGGER.info("Referer is " + backLink);
+        } else {
+            model.addAttribute(BACK_LINK, "/pay-penalty/");
+            LOGGER.info("Refer is sign-out or missing - fallback applied");
+        }
 
         addPhaseBannerToModel(model, signOutService.getSurveyLink());
         return getTemplateName();
