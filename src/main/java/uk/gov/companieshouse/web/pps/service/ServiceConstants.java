edc5e43f948cf2e8adc1dc73dabecaa92bda072c
aonubeze-ch <aonubeze@companieshouse.gov.uk>
2025-07-11 02:21:05 +0100

    SAN-513 Address issue resulting from rebase operation

--- src/main/java/uk/gov/companieshouse/web/pps/service/ServiceConstants.java
+++ src/main/java/uk/gov/companieshouse/web/pps/service/ServiceConstants.java
@@ -3,14 +3,12 @@ package uk.gov.companieshouse.web.pps.service;
 public class ServiceConstants {
 
     public static final String PAYMENT_STATE = "payment_state";
-
     public static final String COMPANY_NAME_ATTR = "companyName";
     public static final String COMPANY_NUMBER_ATTR = "companyNumber";
     public static final String PENALTY_REF_ATTR = "penaltyRef";
     public static final String PENALTY_REF_NAME_ATTR = "penaltyReferenceName";
     public static final String REASON_ATTR = "reasonForPenalty";
     public static final String AMOUNT_ATTR = "outstanding";
-
     public static final String SIGN_OUT_URL_ATTR = "signOutPath";
     public static final String BACK_LINK_URL_ATTR = "backLinkUrl";
 
